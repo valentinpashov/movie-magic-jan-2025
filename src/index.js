c32a138c131f72425fa444299a5fd2f28545f32c
Valentin Pashov <valentinpashov04@gmail.com>
2025-01-29 18:02:25 +0200

    Add about page

--- src/index.js
+++ src/index.js
@@ -14,6 +14,8 @@ app.use('/static', express.static('src/public'));  //Показваме му к�
 app.get('/', (req, res) => {
     res.render('home');
 });
-
+app.get('/about', (req, res) => {
+    res.render('about');
+});
 app.listen(5000, () => console.log('Server is listening on http://localhost:5000...'));
 
