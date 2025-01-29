9018b0e26b1cdd7e67be8f042fb3f75e9600635b
Valentin Pashov <valentinpashov04@gmail.com>
2025-01-29 18:26:10 +0200

    Add home controller

--- src/index.js
+++ src/index.js
@@ -1,23 +1,17 @@
 import express from 'express';
 import handlebars from 'express-handlebars';
+import homeController from './controllers/home-controller.js';
 
 const app = express();
+
+app.use('/static', express.static('src/public'));  //Показваме му къде да търси папкта public
+app.use(homeController);    //Казваме му да използва тези controllers 
 
 app.engine('hbs', handlebars.engine({
     extname: 'hbs',
 }));
 app.set('view engine', 'hbs');
 app.set('views', './src/views');   //Показваме му къде да търси папкта views
-
-app.use('/static', express.static('src/public'));  //Показваме му къде да търси папкта public
-
-app.get('/', (req, res) => {
-    res.render('home');
-});
-
-app.get('/about', (req, res) => {
-    res.render('about');
-});
 
 app.get('*', (req, res) => {
     res.render('404');
