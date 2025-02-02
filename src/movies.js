5d53a7b11b5021b3ebce5bcf9ec7b55678f4aa2d
Valentin Pashov <valentinpashov04@gmail.com>
2025-02-02 21:30:48 +0200

    dynamic details link

--- src/movies.js
+++ src/movies.js
@@ -1,14 +1,16 @@
 export default [
     {
+        id: 1,
         title: 'Jungle Cuise 2',
         genre: 'Adventure',
         description: 'Dreaming about saving countless lives and having another adventure, the feisty English feminist and doctor of botany, Dr Lily Houghton, embarks on a peril-laden mission to change the world. Along with her fashionable brother, MacGregor, Dr Houghton enlists the help of the arrogant, wisecracking riverboat skipper, Captain Frank Wolff, to guide them through the serpentine Amazon River in La Quila, his swift wooden boat. Now, as the intrepid trio ventures deeper and deeper into the heart of an impenetrable green maze, searching for something that cannot be found, a centuries-old curse and the ruthless aristocrat, Prince Joachim, threaten to put an end to their ambitious plans.',
         imageUrl: './static/img/jungle-cruise.jpeg',
     },
     {
+        id: 2,
         title: 'Man of Steel',
         genre: 'Superhero',
         description: 'An alien child is evacuated from his dying world and sent to Earth to live among humans. His peace is threatened when other survivors of his home planet invade Earth.',
-        imageUrl: 'https://www.bing.com/images/search?view=detailV2&ccid=j%2bVbokwT&id=D7741BCFF1FB8BF649931F89E78A13CF80707C33&thid=OIP.j-VbokwTzonbJ-aK_PfImwHaJX&mediaurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR.8fe55ba24c13ce89db27e68afcf7c89b%3frik%3dM3xwgM8TiueJHw%26riu%3dhttp%253a%252f%252fimages6.fanpop.com%252fimage%252fphotos%252f34700000%252fman-of-steel-man-of-steel-34785870-1170-1479.jpg%26ehk%3dPuygj2fq%252fQ8SMhuWGJ9u4gq%252fm9W%252bi%252bKlL2fIN0kw20I%253d%26risl%3d%26pid%3dImgRaw%26r%3d0&exph=1479&expw=1170&q=man+of+steel&simid=608045332927752121&FORM=IRPRST&ck=2D60778E107D663C3D2FA972381F8249&selectedIndex=1&itb=0',
+        imageUrl: './static/img/jungle-cruise.jpeg',
     },
 ] 
