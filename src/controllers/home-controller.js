00d8077c67a19a81dc87c020ff3a17b33277581f
Valentin Pashov <valentinpashov04@gmail.com>
2025-02-03 18:35:10 +0200

    Add getAll to movie-services

--- src/controllers/home-controller.js
+++ src/controllers/home-controller.js
@@ -1,14 +1,16 @@
 import { Router } from "express";
-import movies from "../movies.js"; //temporary solution
+import movieService from "../services/movie-service.js";
 
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.render('home', { movies });
+router.get("/", (req, res) => {
+  const movies = movieService.getAll();
+
+  res.render("home", { movies });
 });
 
-router.get('/about', (req, res) => {
-    res.render('about');
+router.get("/about", (req, res) => {
+  res.render("about");
 });
 
 export default router;
--- src/services/movie-service.js
+++ src/services/movie-service.js
@@ -1,9 +1,27 @@
+import { v4 as uuid } from "uuid"; //генерира id-та
 import movies from "../movies.js";
 
 export default {
-    findOne(movieId) {
-        const result = movies.find(movie => movie.id === movieId);
+  getAll() {
+    return movies;
+  },
 
-        return result;
-    }
-}
+
+  
+  findOne(movieId) {
+    const result = movies.find((movie) => movie.id === movieId);
+
+    return result;
+  },
+
+  create(movieData) {
+    const newId = uuid();
+
+    movies.push({
+      id: newId,
+      ...movieData,
+    });
+
+    return newId;
+  },
+};
