d03b72c9e08734c2f4232810c5fd78228ce25806
Valentin Pashov <valentinpashov04@gmail.com>
2025-02-03 18:34:39 +0200

    Add create movies
    Add uuid

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -3,24 +3,23 @@ import movieService from "../services/movie-service.js";
 
 const movieController = Router();
 
-movieController.get('/create', (req, res) => {
-    res.render('create');
+movieController.get("/create", (req, res) => {
+  res.render("create");
 });
 
+movieController.post("/create", (req, res) => {
+  const newMovie = req.body;
 
-movieController.post('/create', (req,res) => {
-const newMovie = req.body;
+  movieService.create(newMovie);
 
-res.end();
+  res.redirect("/");
 });
 
+movieController.get("/:movieId/details", (req, res) => {
+  const movieId = req.params.movieId;
+  const movie = movieService.findOne(movieId);
 
-movieController.get('/:movieId/details', (req, res) => {
-
-    const movieId = req.params.movieId;
-    const movie = movieService.findOne(movieId);
-
-    res.render('details', { movie });
+  res.render("details", { movie });
 });
 
 export default movieController;
