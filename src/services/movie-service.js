5cf0b0beee820b3f16bb806de23c5b013506c284
Valentin Pashov <valentinpashov04@gmail.com>
2025-02-03 23:29:26 +0200

    Add search by name

--- src/services/movie-service.js
+++ src/services/movie-service.js
@@ -2,12 +2,16 @@ import { v4 as uuid } from "uuid"; //генерира id-та
 import movies from "../movies.js";
 
 export default {
-  getAll() {
-    return movies;
+  getAll(filter = {}) {
+    let result = movies;
+
+    if (filter.search) {
+      result = result.filter((movie) => movie.title.toLocaleLowerCase().includes(filter.search.toLocaleLowerCase()));
+    }
+
+    return result;
   },
 
-
-  
   findOne(movieId) {
     const result = movies.find((movie) => movie.id === movieId);
 
@@ -18,7 +22,7 @@ export default {
     const newId = uuid();
 
     movies.push({
-      id: newId, 
+      id: newId,
       ...movieData,
       rating: Number(movieData.rating),
     });
