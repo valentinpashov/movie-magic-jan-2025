3edfa075fbad9dc8e63906b53ea719c10f0dbd34
Valentin Pashov <valentinpashov04@gmail.com>
2025-02-03 22:47:31 +0200

    Add search page

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -3,23 +3,27 @@ import movieService from "../services/movie-service.js";
 
 const movieController = Router();
 
-movieController.get("/create", (req, res) => {
-  res.render("create");
+movieController.get('/search', (req, res) => {
+  res.render('search');
 });
 
-movieController.post("/create", (req, res) => {
+movieController.get('/create', (req, res) => {
+  res.render('create');
+});
+
+movieController.post('/create', (req, res) => {
   const newMovie = req.body;
 
   movieService.create(newMovie);
 
-  res.redirect("/");
+  res.redirect('/');
 });
 
-movieController.get("/:movieId/details", (req, res) => {
+movieController.get('/:movieId/details', (req, res) => {
   const movieId = req.params.movieId;
   const movie = movieService.findOne(movieId);
 
-  res.render("details", { movie });
+  res.render('details', { movie });
 });
 
 export default movieController;
