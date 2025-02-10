8f3fecec2ac37bb44d6481d7c5ebaa78987f98d7
Valentin Pashov <valentinpashov04@gmail.com>
2025-02-10 23:13:02 +0200

    Add attach cast page

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -26,7 +26,11 @@ movieController.get('/:movieId/details',async (req, res) => {
   const movieId = req.params.movieId;
   const movie =await movieService.getOne(movieId);
 
-  res.render('details', { movie });
+  res.render('movie/details', { movie });
+});
+
+movieController.get('/:movieId/attach-cast', (req,res) => {
+res.render('movie/attach-cast');
 });
 
 export default movieController;
