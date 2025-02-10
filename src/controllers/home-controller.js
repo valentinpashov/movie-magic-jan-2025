ce4ae671c901c637b98a80e6b25140cf60e410fb
Valentin Pashov <valentinpashov04@gmail.com>
2025-02-10 20:04:40 +0200

    Refactor home page to use db

--- src/controllers/home-controller.js
+++ src/controllers/home-controller.js
@@ -3,9 +3,15 @@ import movieService from "../services/movie-service.js";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  const movies = movieService.getAll();
+router.get("/",async (req, res) => {
+  // !Second solution use .lead() on query to get plain object
+  const movies =await movieService.getAll();
 
+  // !First solution -convert documents to objects
+  //Convert documents to plain objects
+  //const plainMovies = movies.map(m => m.toObject());
+
+// !Third solution is to use: runtimeOptions:{allowProtoPropertiesByDefault: true,},
   res.render("home", { movies });
 });
 
--- src/services/movie-service.js
+++ src/services/movie-service.js
@@ -1,26 +1,26 @@
 import { v4 as uuid } from "uuid"; //генерира id-та
 import movies from "../movies.js";
+import Movie from "../models/Movie.js";
 
 export default {
   getAll(filter = {}) {
-    let result = movies;
+  // let result =Movie.find({});   // .lean() дава данните под форма на стандартни обекти
 
-    if (filter.search) {
-      result = result.filter(movie => movie.title.toLowerCase().includes(filter.search.toLowerCase()));
-    }
+  //  if (filter.search) {
+  //    result = result.filter(movie => movie.title.toLowerCase().includes(filter.search.toLowerCase()));
+  //  }
+  //  if (filter.genre) {
+  //    result = result.filter(movie => movie.genre.toLowerCase() === filter.genre.toLowerCase());
+  //  }
+  //  if (filter.year) {
+  //    result = result.filter(movie => movie.year === filter.year);
+  //  }
 
-    if (filter.genre) {
-      result = result.filter(movie => movie.genre.toLowerCase() === filter.genre.toLowerCase());
-    }
-    if (filter.year) {
-      result = result.filter(movie => movie.year === filter.year);
-    }
-
-    return result;
+    return Movie.find({});
   },
 
-  findOne(movieId) {
-    const result = movies.find((movie) => movie.id === movieId);
+  getOne(movieId) {
+    const result = Movie.findById(movieId);
 
     return result;
   },
