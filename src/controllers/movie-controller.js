2135302bc96a7409b1a73494d3c68f4612bd557f
Valentin Pashov <valentinpashov04@gmail.com>
2025-02-10 23:17:38 +0200

    Show movie data to attach page

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -3,34 +3,38 @@ import movieService from "../services/movie-service.js";
 
 const movieController = Router();
 
-movieController.get('/search', async (req, res) => {
+movieController.get("/search", async (req, res) => {
   const filter = req.query;
-  const movies =await movieService.getAll(filter);
+  const movies = await movieService.getAll(filter);
 
-  res.render('search', { movies, filter });
+  res.render("search", { movies, filter });
 });
 
-movieController.get('/create', (req, res) => {
-  res.render('create');
+movieController.get("/create", (req, res) => {
+  res.render("create");
 });
 
-movieController.post('/create',async (req, res) => {
+movieController.post("/create", async (req, res) => {
   const newMovie = req.body;
 
   await movieService.create(newMovie);
 
-  res.redirect('/');
+  res.redirect("/");
 });
 
-movieController.get('/:movieId/details',async (req, res) => {
+movieController.get("/:movieId/details", async (req, res) => {
   const movieId = req.params.movieId;
-  const movie =await movieService.getOne(movieId);
+  const movie = await movieService.getOne(movieId);
 
-  res.render('movie/details', { movie });
+  res.render("movie/details", { movie });
 });
 
-movieController.get('/:movieId/attach-cast', (req,res) => {
-res.render('movie/attach-cast');
+movieController.get("/:movieId/attach-cast",async (req, res) => {
+  const movieId = req.params.movieId;
+  const movie = await movieService.getOne(movieId);
+
+
+  res.render("movie/attach-cast", {movie});
 });
 
 export default movieController;
