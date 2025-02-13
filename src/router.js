0cbae7b44050e75b80be5efa21ae643b6ae5d65d
Valentin Pashov <valentinpashov04@gmail.com>
2025-02-13 10:39:44 +0200

    Add auth controller

--- src/router.js
+++ src/router.js
@@ -3,12 +3,15 @@ import { Router } from 'express';
 import homeController from './controllers/home-controller.js';
 import movieController from './controllers/movie-controller.js';
 import castController from './controllers/cast-controller.js'
+import authController from './controllers/auth-controller.js';
 
 const routes = Router();
 
 routes.use(homeController);
 routes.use('/movies', movieController);
 routes.use('/casts', castController);
+routes.use('/auth', authController);
+
 
 
 routes.get('*', (req, res) => {    //За всяка страница, която не е обособена към сайта
