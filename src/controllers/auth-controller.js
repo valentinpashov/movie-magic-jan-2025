af0c10eae853419dfd0c83dee109088144412e56
Valentin Pashov <valentinpashov04@gmail.com>
2025-02-13 12:41:24 +0200

    Add login functionality and jwt generate

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -3,20 +3,35 @@ import authService from "../services/auth-service.js";
 
 const authController = Router();
 
-authController.get('/register', (req, res) => {
+//! Register
+authController.get("/register", (req, res) => {
   res.render("auth/register");
 });
 
-authController.post('/register', async (req, res) => {
+authController.post("/register", async (req, res) => {
   const userData = req.body;
 
   await authService.register(userData);
 
-  res.redirect('/auth/login');
+  res.redirect("/auth/login");
 });
 
-authController.get('/login', (req,res) => {
-    res.render('auth/login');
-})
+//! Login
+authController.get("/login", (req, res) => {
+  res.render("auth/login");
+});
+
+authController.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const token = await authService.login(email, password);
+    console.log(token);
+  } catch (err) {
+    console.log(err.message);
+    return res.redirect("/404");
+  }
+
+  res.redirect("/");
+});
 
 export default authController;
--- src/services/auth-service.js
+++ src/services/auth-service.js
@@ -1,7 +1,33 @@
 import User from "../models/User.js";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
+const SECRET ='dgs37afscBLPuNgyRRmxJ8c3FgpRzhddR6x1tJribt8b3HV7icKoQR5O6uwMsm2';
 export default {
-    register(userData){
-        return User.create(userData);
+  register(userData) {  //Свързано с auth-controller.js //! Register
+    return User.create(userData);
+  },
+  async login(email, password) {       //Свързано с auth-controller.js //! Login
+    const user = await User.findOne({ email }); //Взимаме данните на user-a
+
+    //Check user exists
+    if (!user) {
+      throw new Error("Invalid email or password!");
     }
-} 
+
+    //Check password is correct
+    const isValid = await bcrypt.compare(password, user.password);
+    if(!isValid){
+        throw new Error("Invalid email or password!");
+    }
+
+    //Generate token
+    const payload = {
+        id: user.id,
+        email: user.email,
+    };
+    const token = jwt.sign(payload, SECRET, {expiresIn:'2h'});
+    //return token
+    return token;
+  },
+};
