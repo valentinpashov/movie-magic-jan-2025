1036824d3271d8a88539d5a5a2d44e3bb8219210
Valentin Pashov <valentinpashov04@gmail.com>
2025-02-13 10:55:16 +0200

    Send data to register endpoint

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -6,4 +6,12 @@ authController.get("/register", (req, res) => {
   res.render("auth/register");
 });
 
+authController.post('/register', async (req,res) => {
+    const userData = req.body;
+
+    console.log(userData);
+
+    res.end();
+});
+
 export default authController;
